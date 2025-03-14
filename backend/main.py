42fb1031a4ab9b7a1e62dbb99eb9518094d055aa
XinghanGuo1019 <hxcgxh@foxmail.com>
2025-03-14 10:12:51 +0100

    test render

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ async def test_endpoint():
 # CORS配置
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
